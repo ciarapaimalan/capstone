8d9c9f0f5b607fc51c277fab3014ca9bdc3f870a
ciarapaimalan <59484700+ciarapaimalan@users.noreply.github.com>
2020-03-05 14:08:33 +0800

    Update UserHomepage.php

--- UserHomepage.php
+++ UserHomepage.php
@@ -1,10 +1,6 @@
 <?php
 include ('MySQL.php');
 session_start();
-if (!isset($_SESSION['username']) || (trim($_SESSION['username']) == '')) {
-    header('location:index.php');
-    exit();
-}
 ?>
 <!DOCTYPE html>
 <html lang="en" >
