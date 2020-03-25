f11ebbfbceb828f3ad1914fd09645857547fbc12
ciarapaimalan <59484700+ciarapaimalan@users.noreply.github.com>
2020-03-26 03:07:06 +0800

    Update UsersSchedule.php

--- UsersSchedule.php
+++ UsersSchedule.php
@@ -22,6 +22,8 @@ if (!isset($_SESSION['username']) || (trim($_SESSION['username']) == '')) {
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
         <link rel="stylesheet" href="./table.css">
+        <link rel="icon" href="usthlogo.png">
+
 
         <style>
             .select:hover {background-color:#f5f5f5;}
