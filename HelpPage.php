1c038c57a14e426946bff5aed9009cd1430ecbf7
ciarapaimalan <59484700+ciarapaimalan@users.noreply.github.com>
2020-03-26 02:35:32 +0800

    Update HelpPage.php

--- HelpPage.php
+++ HelpPage.php
@@ -19,6 +19,8 @@ if (!isset($_SESSION['username']) || (trim($_SESSION['username']) == '')) {
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
         <!--dropwdown li-->
+        <link rel="icon" href="usthlogo.png">
+
         
         <style>
             .select:hover {background-color:#f5f5f5;}
