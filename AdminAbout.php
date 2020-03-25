1c23de492e667be0bdea2b259f57a6528f69c368
ciarapaimalan <59484700+ciarapaimalan@users.noreply.github.com>
2020-03-26 02:11:39 +0800

    Update AdminAbout.php
    added link rel icon

--- AdminAbout.php
+++ AdminAbout.php
@@ -18,6 +18,7 @@ if (!isset($_SESSION['username']) || (trim($_SESSION['username']) == '')) {
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="icon" href="usthlogo.png">
 
         <style>
             h2 {
