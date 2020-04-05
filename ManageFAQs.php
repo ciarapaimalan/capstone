530e7502eb22fc8d31c41a99d328bcd5bd0f556b
ciarapaimalan <59484700+ciarapaimalan@users.noreply.github.com>
2020-04-05 23:40:12 +0800

    Update ManageFAQs.php

--- ManageFAQs.php
+++ ManageFAQs.php
@@ -7,6 +7,35 @@ if (!isset($_SESSION['username']) || (trim($_SESSION['username']) == '')) {
     exit();
 }
 ?>
+<?php
+if (isSet($_POST['Export'])) {
+
+    $sqlSelect = "SELECT * FROM FAQs ";
+    $result = mysqli_query($mysqlconn, $sqlSelect);
+
+    $num_column = mysqli_num_fields($result);
+
+    $csv_header = '';
+    for ($i = 0; $i < $num_column; $i++) {
+        $csv_header .= '"' . mysqli_fetch_field_direct($result, $i)->name . '",';
+    }
+    $csv_header .= "\n";
+
+    $csv_row = '';
+    while ($row = mysqli_fetch_row($result)) {
+        for ($i = 0; $i < $num_column; $i++) {
+            $csv_row .= '"' . $row[$i] . '",';
+        }
+        $csv_row .= "\n";
+    }
+
+    /* Download as CSV File */
+    header('Content-type: application/csv');
+    header('Content-Disposition: attachment; filename=FAQS.csv');
+    echo $csv_header . $csv_row;
+    exit;
+}
+?>
 
 <html lang="en" >
     <head>
